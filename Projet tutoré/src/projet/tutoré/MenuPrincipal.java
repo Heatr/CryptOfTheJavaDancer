e62eb60493261ad7256ac4464758ee3e1596c3c3
RpWNeXus <62759354+RpWNeXus@users.noreply.github.com>
2020-11-06 22:00:04 +0100

    Projet tutoré version 1
    Ajout des options fermeture de la fenêtre sur l'appuie du bouton quitter.
    Ouverture d'une nouvelle fenêtre qui contiendra la carte et tout les autres éléments du jeu lorsque l'on appuie sur le bouton nouvelle partie
    Suppression du bouton options

--- Projet tutoré/src/projet/tutoré/MenuPrincipal.java
+++ Projet tutoré/src/projet/tutoré/MenuPrincipal.java
@@ -7,17 +7,25 @@ package projet.tutoré;
 
 
 
+import javafx.application.Platform;
+import static javafx.application.Platform.exit;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
+import javafx.scene.Group;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 
 /**
  *
@@ -36,25 +44,50 @@ public class MenuPrincipal extends Parent{
         this.getChildren().add(gridpane);
         nouvellePartie.setLayoutX(900);
         nouvellePartie.setLayoutY(800);
-        options.setLayoutX(900);
-        options.setLayoutY(850);
+        //options.setLayoutX(900);
+        //options.setLayoutY(850);
         quitter.setLayoutX(900);
         quitter.setLayoutY(900);
 
         nouvellePartie.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        options.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        //options.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         quitter.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         quitter.setStyle("-fx-font-size: 15pt");
-        options.setStyle("-fx-font-size: 15pt");
+        //options.setStyle("-fx-font-size: 15pt");
         nouvellePartie.setStyle("-fx-font-size: 15pt");
      
         this.getChildren().add(nouvellePartie);
-        this.getChildren().add(options);
+        //this.getChildren().add(options);
         this.getChildren().add(quitter);
+        
+        quitter.setOnMousePressed(new EventHandler<MouseEvent>(){
+        @Override
+        public void handle(MouseEvent me){
+        appuyerQuitter();
+        }
+        });
+        
+        nouvellePartie.setOnMousePressed(new EventHandler<MouseEvent>(){
+        @Override
+        public void handle(MouseEvent me){
+        appuyerNewWindows();
+        }
+        });
     }
     
    Button nouvellePartie = new Button("Nouvelle Partie");
-   Button options = new Button("Options");
+   //Button options = new Button("Options");
    Button quitter = new Button("Quitter");
     
+   public void appuyerQuitter(){
+      quitter.setOnAction(e -> Platform.exit());
+   }
+   
+   public void appuyerNewWindows(){
+       Group root = new Group();
+       Stage jeu = new Stage();
+       jeu.setTitle("Crypt of the NecroDancer");
+       jeu.setScene(new Scene(root,1920,1020));
+       jeu.show();
+   }
 }
