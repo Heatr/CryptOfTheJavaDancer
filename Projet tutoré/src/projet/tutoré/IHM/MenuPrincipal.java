9837d10066e2133f87af4c9ec532414bb0cba7cb
RpWNeXus <d.morin2000@laposte.net>
2020-12-02 23:03:11 +0100

    update des boutons du main
    modification du CSS et des coordonnées des boutons du menu principal

--- Projet tutoré/src/projet/tutoré/IHM/MenuPrincipal.java
+++ Projet tutoré/src/projet/tutoré/IHM/MenuPrincipal.java
@@ -51,9 +51,9 @@ public class MenuPrincipal extends Parent{
         mainmenu.setPreserveRatio(true);
         gridpane.add(mainmenu, 1, 0);
         this.getChildren().add(gridpane);
-        nouvellePartie.setLayoutX(700);
-        nouvellePartie.setLayoutY(800);
-        quitter.setLayoutX(800);
+        nouvellePartie.setLayoutX(750);
+        nouvellePartie.setLayoutY(850);
+        quitter.setLayoutX(850);
         quitter.setLayoutY(900);
 
         nouvellePartie.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
