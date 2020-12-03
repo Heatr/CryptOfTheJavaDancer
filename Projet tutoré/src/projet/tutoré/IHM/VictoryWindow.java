2026c3f8f404559cf39e392f370575d01220e785
SlavKick <louisbalivet89@hotmail.com>
2020-12-03 13:46:34 +0100

    Ecran de fin amélioré
    L'écran de fin est maintenant présentable

--- Projet tutoré/src/projet/tutoré/IHM/VictoryWindow.java
+++ Projet tutoré/src/projet/tutoré/IHM/VictoryWindow.java
@@ -32,32 +32,27 @@ public class VictoryWindow extends Parent {
     
     public VictoryWindow(Group r){
         GridPane gridpane = new GridPane();
+        ImageView victory = new ImageView(new
+        Image(MenuPrincipal.class.getResourceAsStream("victory.jpg")) {});
+        victory.setFitHeight(1095);
+        victory.setPreserveRatio(true);
+        gridpane.add(victory, 1, 0);
         this.getChildren().add(gridpane);
         //options.setLayoutX(900);
         //options.setLayoutY(850);
-        quitter.setLayoutX(875);
-        quitter.setLayoutY(700);
+        quitter.setLayoutX(1180); //en taille normale, c'était 1285
+        quitter.setLayoutY(500);
 
         //options.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         quitter.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        quitter.setStyle("-fx-font-size: 20pt");
+        getStylesheets().add(getClass().getResource("boutonVictoire.css").toExternalForm());
+        //quitter.setStyle("-fx-font-size: 20pt");
         //options.setStyle("-fx-font-size: 15pt");
-        
-        victoire1.setLayoutX(800);
-        victoire1.setLayoutY(300);
-        victoire2.setLayoutX(700);
-        victoire2.setLayoutY(400);
-        
-        victoire1.setStyle("-fx-font-size: 30pt");
-        victoire2.setStyle("-fx-font-size: 30pt");
-        
-        victoire1.setTextFill(Color.WHITE);
-        victoire2.setTextFill(Color.WHITE);
+
      
         //this.getChildren().add(options);
         this.getChildren().add(quitter);
-        this.getChildren().add(victoire1);
-        this.getChildren().add(victoire2);
+
         
         quitter.setOnMousePressed(new EventHandler<MouseEvent>(){
         @Override
@@ -68,8 +63,7 @@ public class VictoryWindow extends Parent {
     }
     
     Button quitter = new Button("Quitter");
-    Label victoire1 = new Label("Félicitations !");
-    Label victoire2 = new Label("Vous avez atteint la sortie !");
+
     
     /**
     * Ferme le jeu
